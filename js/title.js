640f9dd548ab5037ea9ff2dee4877fbd38c03972
18302468380 <897283172@qq.com>
2020-06-20 16:53:26 +0800

    关于我们和殡仪馆已完成

--- js/title.js
+++ js/title.js
@@ -1,53 +1,71 @@
    // 顶部导航栏滑动
    $(document).ready(function () {
-    var mySwiper = new Swiper('.swiper-container', {
-      loop: true,
-      autoplay: true,
-      // 如果需要分页器
-      pagination: {
-        el: '.swiper-pagination',
-      },
+     var mySwiper = new Swiper('.swiper-container', {
+       loop: true,
+       autoplay: true,
+       // 如果需要分页器
+       pagination: {
+         el: '.swiper-pagination',
+       },
 
-      // 如果需要前进后退按钮
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+       // 如果需要前进后退按钮
+       navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+       },
 
-      // 如果需要滚动条
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
+       // 如果需要滚动条
+       scrollbar: {
+         el: '.swiper-scrollbar',
+       },
+     })
+     // 顶部特效
+     $(".MenuIndex-title").mouseover(function () {
+       $(this).addClass('MenuIndexHover')
+     });
+
+     $(".MenuIndex-title").mouseout(function () {
+       $(this).removeClass('MenuIndexHover')
+     });
+     // 下拉特效
+     $(".about-item").mouseover(function () {
+       $(this).addClass('about-item-hover')
+     });
+
+     $(".about-item").mouseout(function () {
+       $(this).removeClass('about-item-hover')
+     });
+     // 关于我们
+     $(".about").mouseover(function () {
+       $("#about").css('visibility', 'visible')
+     })
+     $(".about").mouseout(function () {
+      $("#about").css('visibility', 'hidden')
     })
-    // 顶部特效
-    $(".MenuIndex-title").mouseover(function () {
-      $(this).addClass('MenuIndexHover')
-    });
-
-    $(".MenuIndex-title").mouseout(function () {
-      $(this).removeClass('MenuIndexHover')
-    });
-    // 下拉特效
-    $(".about-item").mouseover(function () {
-      $(this).addClass('about-item-hover')
-    });
-
-    $(".about-item").mouseout(function () {
-      $(this).removeClass('about-item-hover')
-    });
-    // 关于我们
-    $(".about").mouseover(function () {
-      $(".about-wrap").css('visibility', 'visible')
+     $("#about").hover(
+       function () {
+         $(this).css("visibility", "visible");
+       },
+       function () {
+         $(this).css("visibility", "hidden");
+       }
+     );
+     // 解决方案
+     $(".solution").mouseover(function () {
+       $("#solution").css('visibility', 'visible')
+     })
+     $(".solution").mouseout(function () {
+      $("#solution").css('visibility', 'hidden')
     })
-    $(".about-wrap").hover(
-      function () {
-        $(this).css("visibility", "visible");
-      },
-      function () {
-        $(this).css("visibility", "hidden");
-      }
-    );
+     $("#solution").hover(
+       function () {
+         $(this).css("visibility", "visible");
+       },
+       function () {
+         $(this).css("visibility", "hidden");
+       }
+     );
 
 
 
-  });
+   });
